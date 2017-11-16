ab920d92a11bca0fd25f5adaa88306ab1fe547b2
yli <460255770@qq.com>
2017-11-16 17:01:46 +0800

    spring boot try

---
+++ src/main/java/com/mybatis/MacwarApplication.java
@@ -0,0 +1,11 @@
+package com.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacwarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MacwarApplication.class, args);
+    }
+}
